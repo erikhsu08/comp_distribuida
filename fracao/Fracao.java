789bff0f725670bcfeb43643be55af63c3651875
Erik Hsu <111096802+erikhsu08@users.noreply.github.com>
2024-09-13 21:49:27 -0300

    Update Fracao.java

--- fracao/Fracao.java
+++ fracao/Fracao.java
@@ -1,3 +1,8 @@
+/* TURMA: 06G11
+NOME: ERIK SAMUEL VIANA HSU 							RA:10403109
+NOME: THIAGO SHIHAN CARDOSO TOMA						RA:10400764
+*/
+
 import java.io.Serializable;
 
 public class Fracao implements Serializable {
